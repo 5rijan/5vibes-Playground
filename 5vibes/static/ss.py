f083674f6342a097525fcf29800c91192a8f0d50
Srijan Chaudhary <87299199+5rijan@users.noreply.github.com>
2024-01-30 07:43:43 +1100

    Delete 5vibes/static/ss.py

--- 5vibes/static/ss.py
+++
@@ -1,5 +0,0 @@
-locations = "['world','algeria','argentina','argentina/buenos-aires', 'australia','australia/adelaide','australia/brisbane',...]"
-locations_without_quotes = locations.replace("'", "")
-
-# Now you can print the modified string
-print(locations_without_quotes)
